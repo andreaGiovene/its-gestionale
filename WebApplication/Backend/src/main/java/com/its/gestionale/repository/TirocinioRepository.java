af0c50860867f70b0328b3bad7476dae59f02848
Andrea Giovene <andrea.giovene@edu-its.it>
2026-03-31 14:17:25 +0200

    added TirocinioRepository

--- WebApplication/Backend/src/main/java/com/its/gestionale/repository/TirocinioRepository.java
+++ WebApplication/Backend/src/main/java/com/its/gestionale/repository/TirocinioRepository.java
@@ -1,15 +1,16 @@
 package com.its.gestionale.repository;
 
-import com.its.gestionale.entity.Tirocinio;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
+import com.its.gestionale.entity.Tirocinio;
 
 @Repository
 public interface TirocinioRepository extends JpaRepository<Tirocinio, Integer> {
     List<Tirocinio> findByAllievoId(Integer allievoId);
     List<Tirocinio> findByAziendaId(Integer aziendaId);
-    List<Tirocinio> findByDataInizioGreaterThanEqualAndDataFineBeforeOrEqual(LocalDate start, LocalDate end);
+    List<Tirocinio> findByDataInizioGreaterThanEqualAndDataFineLessThanEqual(LocalDate start, LocalDate end);
 }
